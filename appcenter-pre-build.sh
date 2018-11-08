0100bc3fb84d0105637fabf675a6805de0ec23da
Jeff Francisco <jeff.francisco7@gmail.com>
2018-11-07 20:35:10 -0500

    Fix error in pre build script

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -6,4 +6,4 @@ sed -i '' "s/APPCENTER_SECRET/$APPCENTER_SECRET/g" $AppCenterConfigFile
 # Update version number
 Plist=$APPCENTER_SOURCE_DIRECTORY/ios/learncash/Info.plist
 
-/usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString 1.1.${APPCENTER_BUILD_ID}" $PLIST
+/usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString 1.1.${APPCENTER_BUILD_ID}" $Plist
